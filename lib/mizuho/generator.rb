b79e454a15689490651bb919cabbf6fa0991e3c1
Hongli Lai (Phusion) <hongli@phusion.nl>
2008-07-19 18:24:18 +0200

    Refactoring: prepare to implement multi-page support.

--- lib/mizuho/generator.rb
+++ lib/mizuho/generator.rb
@@ -8,19 +8,22 @@ class GenerationError < StandardError
 end
 
 class Generator
-	def initialize(input, output = nil, template = nil)
+	def initialize(input, output = nil, template = nil, multi_page = false)
 		@input = input
 		if output
-			@output = output
+			@output_name = output
 		else
-			@output = File.expand_path(input.sub(/(.*)\..*$/, '\1.html'))
+			@output_name = File.expand_path(input.sub(/(.*)\..*$/, '\1'))
 		end
 		@template = template
+		@multi_page = multi_page
 	end
 	
 	def start
-		run_asciidoc(@input, @output)
-		apply_template(@output, @template) if @template
+		run_asciidoc(@input, "#{@output_name}.html")
+		if @template
+			apply_template("#{@output_name}.html", @template, @multi_page)
+		end
 	end
 
 private
@@ -32,13 +35,18 @@ private
 		end
 	end
 	
-	def apply_template(file, template_file)
-		parser = Parser.new(file)
-		template = Template.new(template_file,
-			:title => parser.title,
-			:table_of_contents => parser.table_of_contents,
-			:contents => parser.contents)
-		template.save(file)
+	def apply_template(asciidoc_file, template_file, multi_page)
+		parser = Parser.new(asciidoc_file)
+		if multi_page
+			File.unlink(asciidoc_file)
+			puts "TODO"
+		else
+			template = Template.new(template_file,
+				:title => parser.title,
+				:table_of_contents => parser.table_of_contents,
+				:contents => parser.contents)
+			template.save(asciidoc_file)
+		end
 	end
 end
 
