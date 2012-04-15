a525d0543fbd006efb16081810f4a9f5da993042
Hongli Lai (Phusion) <hongli@phusion.nl>
2012-04-15 12:39:59 +0200

    Add tests for IdMap and fix bugs in it.

--- lib/mizuho/generator.rb
+++ lib/mizuho/generator.rb
@@ -1,3 +1,23 @@
+# Copyright (c) 2008-2012 Hongli Lai
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 require 'nokogiri'
 require 'mizuho'
 require 'mizuho/source_highlight'
@@ -36,11 +56,12 @@ class Generator
 		transform(@output_file)
 		if @commenting_system
 			@id_map.save(@id_map_file)
-			if @id_map.fuzzy_count > 0
-				warn "Warning: #{@id_map.fuzzy_count} fuzzy ID(s)"
+			stats = @id_map.stats
+			if stats[:fuzzy] > 0
+				warn "Warning: #{stats[:fuzzy]} fuzzy ID(s)"
 			end
-			if @id_map.orphaned_count > 0
-				warn "Warning: #{@id_map.orphaned_count} orphaned ID(s)"
+			if stats[:orphaned] > 0
+				warn "Warning: #{stats[:orphaned]} unused ID(s)"
 			end
 		end
 	end
---
+++ test/id_map_spec.rb
@@ -0,0 +1,188 @@
+# Copyright (c) 2012 Hongli Lai
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
+require 'stringio'
+require 'mizuho/id_map'
+
+describe Mizuho::IdMap do
+	before :each do
+		@id_map = Mizuho::IdMap.new
+	end
+
+	describe "#associate" do
+		describe "if the given title is not in the map" do
+			describe "if no similar titles exist in the map" do
+				before :each do
+					@id1 = @id_map.associate "Installation"
+					@id2 = @id_map.associate "Installation on Linux"
+					@id3 = @id_map.associate "Configuration"
+				end
+
+				it "returns a new unique ID" do
+					@id1.should_not == @id2
+					@id1.should_not == @id3
+				end
+
+				it "marks the entry as associated" do
+					@id_map["Installation"].should be_associated
+					@id_map["Installation on Linux"].should be_associated
+					@id_map["Configuration"].should be_associated
+				end
+
+				it "doesn't mark the corresponding entry as fuzzy" do
+					@id_map["Installation"].should_not be_fuzzy
+					@id_map["Installation on Linux"].should_not be_fuzzy
+					@id_map["Configuration"].should_not be_fuzzy
+				end
+			end
+
+			describe "if one or more similar titles exist in the map" do
+				before :each do
+					@entry1 = @id_map.add("Installation using a tarball", nil, false, false)
+					@entry2 = @id_map.add("Installation using a Linux tarball", nil, false, false)
+				end
+
+				it "associates with the most similar title and returns its ID" do
+					@id3 = @id_map.associate "Installation using tarball"
+					@id3.should == @entry1.id
+					@entry1.title.should == "Installation using tarball"
+				end
+
+				it "only associates with a title that hasn't been associated before" do
+					@entry1.associated = true
+					@id3 = @id_map.associate "Installation using tarball"
+					@id3.should == @entry2.id
+				end
+
+				it "marks the corresponding entry as associated" do
+					@id_map.associate "Installation using tarball"
+					@id_map["Installation using tarball"].should be_associated
+				end
+
+				it "marks the corresponding entry as fuzzy" do
+					@id_map.associate "Installation using tarball"
+					@id_map["Installation using tarball"].should be_fuzzy
+				end
+			end
+
+			specify "title matching is case-insensitive" do
+				entry1 = @id_map.add("INSTALLATION USING A TARBALL", nil, false, false)
+				id = @id_map.associate "installation using tarball"
+				id.should == entry1.id
+			end
+		end
+
+		describe "if the given title is in the map and it hasn't been associated before" do
+			before :each do
+				@entry = @id_map.add("Installation", nil, false, false)
+			end
+
+			it "returns that title's previous ID" do
+				id = @id_map.associate "Installation"
+				id.should == @entry.id
+			end
+
+			it "marks the entry as associated" do
+				@id_map.associate "Installation"
+				@entry.should be_associated
+			end
+
+			it "preserves the entry's fuzziness" do
+				@id_map.associate "Installation"
+				@entry.should_not be_fuzzy
+
+				@entry2 = @id_map.add("Installation 2", nil, true, false)
+				@id_map.associate "Installation 2"
+				@entry2.should be_fuzzy
+			end
+		end
+
+		describe "if the given title is in the map and it has been associated before" do
+			it "raises an error" do
+				@entry = @id_map.add("Installation", nil, false, true)
+				lambda { @id_map.associate "Installation" }.should raise_error(Mizuho::IdMap::AlreadyAssociatedError)
+			end
+		end
+	end
+
+	describe "loading" do
+		before :each do
+			@io = StringIO.new
+		end
+
+		it "works" do
+			@io.puts "# This is a comment."
+			@io.puts ""
+			@io.puts "Installation	=>	installation-1"
+			@io.puts "Configuration	=>	configuration-2"
+			@io.puts "# fuzzy"
+			@io.puts "Troubleshooting	=>	troubleshooting-1"
+			@io.rewind
+			@id_map.load(@io)
+
+			@id_map.entries.should have(3).items
+			@id_map.entries.each_value { |entry| entry.should_not be_associated }
+			
+			entry = @id_map["Installation"]
+			entry.title.should == "Installation"
+			entry.id.should == "installation-1"
+			entry.should_not be_fuzzy
+
+			entry = @id_map["Configuration"]
+			entry.title.should == "Configuration"
+			entry.id.should == "configuration-2"
+			entry.should_not be_fuzzy
+
+			entry = @id_map["Troubleshooting"]
+			entry.title.should == "Troubleshooting"
+			entry.id.should == "troubleshooting-1"
+			entry.should be_fuzzy
+		end
+	end
+
+	describe "saving" do
+		before :each do
+			@io = StringIO.new
+			@id_map.add "1. Installation", "installation-1", false, true
+			@id_map.add "2. Configuration", "configuration-2", false, true
+			@id_map.add "3. Troubleshooting", "troubleshooting-1", true, true
+			@id_map.add "4. Administration", "administration-1", true, false
+			@id_map.add "5. Uninstallation", "uninstallation-1", false, false
+			@id_map.add "0. Introduction", "intro", false, true
+		end
+
+		it "saves all entries in alphabetical order, marks fuzzy entries as such and puts unassociated (orphaned) entries at the bottom" do
+			@id_map.save(@io)
+			@io.string.should ==
+				Mizuho::IdMap::BANNER +
+				"0. Introduction	=>	intro\n\n" +
+				"1. Installation	=>	installation-1\n\n" +
+				"2. Configuration	=>	configuration-2\n\n" +
+				"# fuzzy\n" +
+				"3. Troubleshooting	=>	troubleshooting-1\n\n" +
+				"\n" +
+				"### These sections appear to have been removed. Please check.\n\n" +
+				"# fuzzy\n" +
+				"4. Administration	=>	administration-1\n\n" +
+				"5. Uninstallation	=>	uninstallation-1\n\n"
+		end
+	end
+end
